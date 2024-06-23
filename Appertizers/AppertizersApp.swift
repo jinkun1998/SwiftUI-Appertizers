457329d0f400caf7beb4aa33b86893d3293033f8
Quoc Thao <quocthao23061998.tg@gmail.com>
2024-06-23 13:00:57 +0700

    Initial Commit

---
+++ Appertizers/AppertizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppertizersApp.swift
+//  Appertizers
+//
+//  Created by Jin on 23/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AppertizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
