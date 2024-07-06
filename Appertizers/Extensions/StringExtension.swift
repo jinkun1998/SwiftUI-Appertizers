077208f940e2fc5d9a0691b1030ad2a44680544b
Quoc Thao <quocthao23061998.tg@gmail.com>
2024-07-06 13:35:04 +0700

    # update account view

---
+++ Appertizers/Extensions/StringExtension.swift
@@ -0,0 +1,8 @@
+//
+//  StringExtension.swift
+//  Appertizers
+//
+//  Created by Jin on 6/7/24.
+//
+
+import Foundation
---
+++ Appertizers/ViewModels/AccountViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AccountViewModel.swift
+//  Appertizers
+//
+//  Created by Jin on 6/7/24.
+//
+
+import Foundation
